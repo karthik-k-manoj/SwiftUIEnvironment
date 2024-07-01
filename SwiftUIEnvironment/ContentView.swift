81e8c2b0bc63fdeeee863f3f48f49768c7cf71e0
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-07-01 10:26:45 +0530

    initial commit

---
+++ SwiftUIEnvironment/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUIEnvironment
+//
+//  Created by Karthik K Manoj on 01/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIEnvironment/SwiftUIEnvironmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIEnvironmentApp.swift
+//  SwiftUIEnvironment
+//
+//  Created by Karthik K Manoj on 01/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIEnvironmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
