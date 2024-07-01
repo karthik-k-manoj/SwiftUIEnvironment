2d07203f743556a2338f9199a634df3a82e5b340
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-07-01 11:17:16 +0530

    implement custom environment key value

--- SwiftUIEnvironment/ContentView.swift
+++ SwiftUIEnvironment/ContentView.swift
@@ -7,15 +7,60 @@
 
 import SwiftUI
 
+// Custom key of type `EnvironmentKey`
+// We need to provide a default value for the it's type
+enum BadgeColorKey: EnvironmentKey {
+    static var defaultValue: Color = .yellow
+}
+
+// Custom value which uses custom key defined above.
+extension EnvironmentValues {
+    var myCustomColor: Color {
+        get {
+            self[BadgeColorKey.self]
+        }
+        
+        set {
+            self[BadgeColorKey.self] = newValue
+        }
+    }
+}
+
+struct Badge: ViewModifier {
+    @Environment(\.myCustomColor) private var badgeColor
+    
+    func body(content: Content) -> some View {
+        content
+            .font(.caption)
+            .foregroundColor(.white)
+            .padding(.horizontal, 5)
+            .padding(.vertical, 2)
+            .background {
+                Capsule(style: .continuous)
+                    .fill(badgeColor)
+            }
+    }
+}
+
+extension View {
+    func badgeColor(_ color: Color) -> some View {
+        environment(\.myCustomColor, color)
+    }
+    
+    func applyBadge() -> some View {
+        modifier(Badge())
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text(3000, format: .number)
+                .applyBadge()
+            Text(4000, format: .number)
+                .applyBadge()
         }
-        .padding()
+        .badgeColor(.green)
     }
 }
 
@@ -24,3 +69,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
